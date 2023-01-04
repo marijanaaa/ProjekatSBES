6719771e6c72ebd0897edfe2d00606816fd93755
marijanaaa <marijanaaastojanovic@gmail.com>
2023-01-04 12:17:01 +0100

    loading json object into spark table - changes

--- SBES/app.py
+++ SBES/app.py
@@ -10,57 +10,23 @@ import argparse
 
 API_KEY = "8efd6a55d157ee2cdaee567709846929faaa8e2620f67f7abf6e34c1db260649"
 otx = OTXv2(API_KEY)
+#sesija za spark
 spark = SparkSession.builder.master("local[1]").appName('SparkByExamples.com').getOrCreate()#app name is just session name
 
+#odavde uzimam jedan otx objekat, tj virus
 data_from_otx = otx.getall_iter(max_page=1)
+#upisujem ga u json fajl
 for i in data_from_otx:
     json_object = json.dumps(i,default=str)
     with open("data_from_otx.json","w")as outfile:
         outfile.write(json_object)
 
-#columns = ["id","indicator","type","created", "content","title","description","expiration","is_active","role"]
-#dataframe = spark.read.json("data_from_otx.json")
-#data_frame = spark.createDataFrame(data)
-#list = dataframe.select("indicators")
-
-schema = StructType([ \
-    StructField("id", StringType(), True), \
-    StructField("name", StringType(), True), \
-    StructField("description", StringType(), True), \
-    StructField("author_name", StringType(), True), \
-    StructField("modified", StringType(), True), \
-    StructField("created", StringType(), True), \
-    StructField("revision", IntegerType(), True), \
-    StructField("tlp", StringType(), True), \
-    StructField("public", IntegerType(), True), \
-    StructField("adversary", StringType(), True), \
-    StructField('indicators', ArrayType(
-        StructType([ \
-            StructField("id", IntegerType(), True), \
-            StructField("indicator", StringType(), True), \
-            StructField("type", StringType(), True), \
-            StructField("created", StringType(), True), \
-            StructField("content", StringType(), True), \
-            StructField("title", StringType(), True), \
-            StructField("description", StringType(), True), \
-            StructField("expiration", StringType(), True), \
-            StructField("is_active", IntegerType(), True), \
-            StructField("role", StringType(), True) \
-            ]) \
-        )), \
-        
-    StructField("tags",ArrayType(StringType()),True), \
-    StructField("targeted_countries",ArrayType(StringType()),True), \
-    StructField("malware_families", ArrayType(StringType()), True), \
-    StructField("attack_ids", ArrayType(StringType()), True), \
-    StructField("referencies", ArrayType(StringType()), True), \
-    StructField("industries", ArrayType(StringType()), True), \
-    StructField("extract_source", ArrayType(StringType()), True), \
-    StructField("more_indicators", BooleanType(), True) 
-  ])
-
-df2 = spark.read.schema(schema).json("data_from_otx.json")
-df2.select("indicators").show()
+df2 = spark.read.json("data_from_otx.json")
+#ispise se schema koja je napravljena u sparku od json obj koji sam ucitala
+#sa alien vaulta a taj obj predstavlja neki vid obj koji opisuje virus
+df2.printSchema()
+#da mi iz liste indikatora ispise polje indikator prvog iz liste
+df2.select(df2["indicators"].getItem("indicator")[0]).show()
 
 if __name__ == "__main__":
     print(f"HEllo, I'm Python VErsion : {sys.version}")
