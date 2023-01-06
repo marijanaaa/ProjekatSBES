583b1b3f2cd1942a622d573320da46b58e829ac1
KatarinaProdanovic <katarina.prodanovic20@gmail.com>
2023-01-06 19:00:53 +0100

    Added pulses from AlienVault

--- SBES/app.py
+++ SBES/app.py
@@ -1,69 +1,52 @@
-import sys
-import findspark
-findspark.init() 
-from OTXv2 import OTXv2, IndicatorTypes
-import pandas as pd
+import requests
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, MapType, BooleanType, ArrayType
-import requests, json
-import argparse
-import is_malicious
+import findspark
+findspark.init()
+# Create a SparkSession
+spark = SparkSession.builder.appName("AlienVault").getOrCreate()
+# Set your AlienVault API key
+api_key = "8efd6a55d157ee2cdaee567709846929faaa8e2620f67f7abf6e34c1db260649"
+# Set the base URL for the AlienVault API
+base_url = "https://otx.alienvault.com"
+# Set the endpoint for the AlienVault API
+endpoint = "/api/v1/pulses/subscribed"
+# Set the headers for the API request
+headers = {
+    "X-OTX-API-KEY": api_key
+}
+# Set the parameters for the API request
+params = {
+    "limit": 3  # Set the number of pulses to retrieve
+}
+# Send the API request
+response = requests.get(base_url + endpoint, headers=headers, params=params)
+# Check the status code of the response
+if response.status_code == 200:
+    # Convert the response to a JSON object
+    import json
+    data = response.json()
+    
+   
+
+    #print(pod)
+    # Extract the list of pulses from the response
+    pulses = data["results"]
+    pod = json.dumps(pulses,default=str)
+    with open("Data.json","w")as outfile:
+        outfile.write(pod)
 
 
-API_KEY = "8efd6a55d157ee2cdaee567709846929faaa8e2620f67f7abf6e34c1db260649"
-OTX_SERVER = 'https://otx.alienvault.com/'
-otx = OTXv2(API_KEY, server=OTX_SERVER)
-#sesija za spark
-spark = SparkSession.builder.master("local[1]").appName('SparkByExamples.com').getOrCreate()#app name is just session name
-
-#odavde uzimam jedan otx objekat, tj virus
-data_from_otx = otx.getall_iter(max_page=5)
-#upisujem ga u json fajl
-for i in data_from_otx:
-    json_object = json.dumps(i,default=str)
-    with open("CTI_Data.json","w")as outfile:
-        outfile.write(json_object)
-
-#df2 = spark.read.json("data_from_otx.json")
-#ispise se schema koja je napravljena u sparku od json obj koji sam ucitala
-#sa alien vaulta a taj obj predstavlja neki vid obj koji opisuje virus
-#df2.printSchema()
-#da mi iz liste indikatora ispise polje indikator prvog iz liste
-#df2.select(df2["indicators"].getItem("indicator")[0]).show()
-
-dataframe = spark.read.json("CTI_Data.json")
+    # Convert the list of pulses to a Spark DataFrame
+    df = spark.read.format("json").option("inferSchema", "true").load("Data.json")
+    df.createOrReplaceTempView("puls")#pravim tabelu koja se zove puls
+   # spark.sql("select count(indicators) from puls").show()#tri pulsa je ucitalo, treba da ispise da ima 3 indicators
+    df.select(df["indicators"].getItem("indicator")[0]).show() #za svaki indicators ce uzeti njegov prvi indicator
+    #spark.sql("select tlp from puls").show()
+    #spark.sql("select indicator from indicators from puls").show()
 #dataframe.printSchema()
-#dataframe.select(dataframe["Source"]).show()
-
-parser = argparse.ArgumentParser(description='OTX CLI Example')
-parser.add_argument('-ip', help='IP eg; 4.4.4.4', required=False)
-parser.add_argument('-host',
-                    help='Hostname eg; www.alienvault.com', required=False)
-parser.add_argument(
-    '-url', help='URL eg; http://www.alienvault.com', required=False)
-parser.add_argument(
-    '-hash', help='Hash of a file eg; 7b42b35832855ab4ff37ae9b8fa9e571', required=False)
-parser.add_argument(
-    '-file', help='Path to a file, eg; malware.exe', required=False)
-
-#private ip adrese
-'''Class A: 10.0.0.0 to 10.255.255.255
-   Class B: 172.16.0.0 to 172.31.255.255
-   Class C: 192.168.0.0 to 192.168.255.255'''
-'''def func(ip):
-    print("------------------")
-    print(ip)
-    splited_ip = ip.split('.')
-    if splited_ip[0] == '10' and int(splited_ip[1]) <= 255 or splited_ip[0] == '172' and int(splited_ip[1]) <= 31 or splited_ip[0] == '192' \
-        and int(splited_ip[1]) <= 168 or splited_ip[0] == '0':
-        print("Private ip")
-        print("------------------")
-    else:
-        args = vars(parser.parse_args(("-ip "+ip).split()))
-        is_malicious.is_malicious(otx, args)
-
-for iterator in dataframe.collect():
-    func(iterator["Source"])'''
-
-if __name__ == "__main__":
-    print(f"HEllo, I'm Python VErsion : {sys.version}")
+    # Print the DataFrame to the console
+   
+else:
+    print("Error:", response.status_code)
+# Stop the SparkSession
+spark.stop()
