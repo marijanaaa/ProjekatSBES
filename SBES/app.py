f3bad59343f57edbdbd899ca48f2ff3ae63f1e2f
marijanaaa <marijanaaastojanovic@gmail.com>
2023-01-31 16:05:22 +0100

    displaying industries, malware families, tags, targeted countries and adversary with matplotlib

--- SBES/app.py
+++ SBES/app.py
@@ -1,7 +1,7 @@
 import requests
-from pyspark.sql import SparkSession
 import findspark
 findspark.init()
+from pyspark.sql import SparkSession
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -19,7 +19,7 @@ headers = {
 }
 # Set the parameters for the API request
 params = {
-    "limit": 100 # Set the number of pulses to retrieve
+    "limit": 300 # Set the number of pulses to retrieve
 }
 # Send the API request
 response = requests.get(base_url + endpoint, headers=headers, params=params)
@@ -35,13 +35,13 @@ if response.status_code == 200:
     # Extract the list of pulses from the response
     pulses = data["results"]
     pod = json.dumps(pulses,default=str)
-    with open("Data.json","w")as outfile:
+    with open("Data1.json","w")as outfile:
         outfile.write(pod)
 
 
     # Convert the list of pulses to a Spark DataFrame
     df = spark.read.format("json").option("inferSchema", "true").load("Data.json")
-    df.createOrReplaceTempView("puls")#pravim tabelu koja se zove puls
+    df.createOrReplaceTempView("pulse")#pravim tabelu koja se zove puls
     #spark.sql("select count(indicators) from puls").show()#tri pulsa je ucitalo, treba da ispise da ima 3 indicators
     
     
@@ -56,36 +56,148 @@ if response.status_code == 200:
          # novi.append(x) 
 
             
-    countries =  spark.sql("select targeted_countries from puls").collect()[0][0]
+    #countries =  spark.sql("select targeted_countries from puls").collect()[0][0]
 
     #print(countries)
-    countries1 = spark.sql("select targeted_countries from puls").collect()
-    my_dictionary = dict.fromkeys(countries)
+    rows = spark.sql("select targeted_countries from pulse").collect()
+    dict = {}
+    for row in rows:
+        result = row.asDict()
+        array = result["targeted_countries"]
+        for country in array:
+            if country in dict.keys():
+                dict[country] += 1
+            else:
+                dict[country] = 1
 
+    print(dict)
+                            
     
-    
-
-   
-    for i in countries1:
-        for x in i:
-            for n in x:
-               if n in my_dictionary.keys():
-                   if my_dictionary[n] == None:
-                        my_dictionary[n] = 1
-                   else:
-                     my_dictionary[n] = my_dictionary[n] + 1
-                   
-    print(my_dictionary)           
-    
-    group_data = list(my_dictionary.values())
-    group_names = list(my_dictionary.keys())
+    group_data = list(dict.values())
+    group_names = list(dict.keys())
     group_mean = np.mean(group_data) 
-
 
 
     fig, ax = plt.subplots()
     ax.barh(group_names, group_data)
+    colors = ( "orange", "cyan", "yellow", "grey", "green",)
+    plt.title("Most targeted countries")
+    plt.savefig("targeted_countries.jpg")
+
+
+    #MALWARE FAMILIES
+    rows = spark.sql("select malware_families from pulse").collect()
+    dict = {}
+    for row in rows:
+        result = row.asDict()
+        array = result["malware_families"]
+        for country in array:
+            if country in dict.keys():
+                dict[country] += 1
+            else:
+                dict[country] = 1
+
+    print(dict)
+                            
+    
+    group_data = list(dict.values())
+    group_names = list(dict.keys())
+    group_mean = np.mean(group_data) 
+
+
+    fig, ax = plt.subplots()
+    ax.barh(group_names, group_data)
+    plt.title("Most common malware families")
+    plt.savefig("malware_families.jpg")
+
+    #INDUSTRIES
+    rows = spark.sql("select industries from pulse").collect()
+    dict = {}
+    for row in rows:
+        result = row.asDict()
+        array = result["industries"]
+        for country in array:
+            if country in dict.keys():
+                dict[country] += 1
+            else:
+                dict[country] = 1
+
+    print(dict)
+                            
+    
+    group_data = list(dict.values())
+    group_names = list(dict.keys())
+    group_mean = np.mean(group_data) 
+
+
+    fig, ax = plt.subplots()
+    ax.barh(group_names, group_data)
+    plt.title("Most attacked industries")
+    plt.savefig("industries.jpg")
+
+    #ADVERSARY
+    rows = spark.sql("select adversary from pulse").collect()
+    dict = {}
+    result = row.asDict()
+    for row in rows:
+        result = row.asDict()
+        print(result)
+        value = result["adversary"]
+        if value == "":
+            continue
+        if value in dict.keys():
+            dict[value] += 1
+        else:
+            dict[value] = 1
+    print(dict)
+                            
+    
+    group_data = list(dict.values())
+    group_names = list(dict.keys())
+    group_mean = np.mean(group_data) 
+
+
+    fig, ax = plt.subplots()
+    ax.barh(group_names, group_data)
+    plt.title("Adversaries")
+    plt.savefig("adversary.jpg")
+
+
+    #TAGS
+    rows = spark.sql("select tags from pulse").collect()
+    dict = {}
+    for row in rows:
+        result = row.asDict()
+        array = result["tags"]
+        for country in array:
+            if country in dict.keys():
+                dict[country] += 1
+            else:
+                dict[country] = 1
+
+    sorted_tuple_list = sorted(dict.items(), key=lambda x:x[1], reverse=True)    
+    sorted_tuple_list = sorted_tuple_list[:10]
+    print(sorted_tuple_list)
+
+    dict={}
+    for tuple in sorted_tuple_list:
+        dict[tuple[0]] = tuple[1]
+
+    print("\nDICT\n");
+    print(dict)
+
+    group_data = list(dict.values())
+    group_names = list(dict.keys())
+    group_mean = np.mean(group_data) 
+
+
+    fig, ax = plt.subplots()
+    ax.barh(group_names, group_data)
+    plt.title("Tags")
+    plt.savefig("tags.jpg")
     plt.show()
+
+
     #print(numIndic)           #print(niz)
     #.za svaki indicators ce uzeti njegov prvi indicator
     #dataNEw = spark.sql("select indicators from puls").collect()
@@ -95,7 +207,7 @@ if response.status_code == 200:
     #spark.sql("select revision from puls").show
 #dataframe.printSchema()
     # Print the DataFrame to the console
-   
+
 else:
     print("Error:", response.status_code)
 # Stop the SparkSession
