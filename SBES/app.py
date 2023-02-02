0c6d6b35fdee910f5de2b55dad89944257405c30
KatarinaProdanovic <katarina.prodanovic20@gmail.com>
2023-02-02 11:32:19 +0100

    Added changes

--- SBES/app.py
+++ SBES/app.py
@@ -1,7 +1,7 @@
 import requests
-from pyspark.sql import SparkSession
 import findspark
 findspark.init()
+from pyspark.sql import SparkSession
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -19,7 +19,7 @@ headers = {
 }
 # Set the parameters for the API request
 params = {
-    "limit": 100 # Set the number of pulses to retrieve
+    "limit": 300 # Set the number of pulses to retrieve
 }
 # Send the API request
 response = requests.get(base_url + endpoint, headers=headers, params=params)
@@ -30,72 +30,135 @@ if response.status_code == 200:
     data = response.json()
     
    
-
-    #print(pod)
     # Extract the list of pulses from the response
     pulses = data["results"]
     pod = json.dumps(pulses,default=str)
-    with open("Data.json","w")as outfile:
+    with open("Data1.json","w")as outfile:
         outfile.write(pod)
 
 
     # Convert the list of pulses to a Spark DataFrame
-    df = spark.read.format("json").option("inferSchema", "true").load("Data.json")
-    df.createOrReplaceTempView("puls")#pravim tabelu koja se zove puls
-    #spark.sql("select count(indicators) from puls").show()#tri pulsa je ucitalo, treba da ispise da ima 3 indicators
-    
-    
-   # br = 0
-    #novi = []
-   # dataNew = df.select(df["indicators"].getItem("indicator")).collect()# svi indikatori  jer je 3 pulsa(imamo 3 indicators)(za svaki indicators, njegovi indicator)
-    #niz nizova indikatora(u tom nizu ima 3 niza))
-    
-   # print(indicator[0])
-   # for i in dataNew: #prolazi kroz taj veliki niz
-       # for x in i:  #x je taj mali niz(mali nizovi u okviru velikog niza)
-         # novi.append(x) 
-
+    df = spark.read.format("json").option("inferSchema","true") .load("Data.json")
+    df.createOrReplaceTempView("pulse")#pravim tabelu koja se zove puls
             
-    countries =  spark.sql("select targeted_countries from puls").collect()[0][0]
+    def plot(dict, parameter):
+        group_data = list(dict.values())
+        group_names = list(dict.keys())
+        group_mean = np.mean(group_data) 
 
-    #print(countries)
-    countries1 = spark.sql("select targeted_countries from puls").collect()
-    my_dictionary = dict.fromkeys(countries)
+        fig, ax = plt.subplots()
+        ax.barh(group_names, group_data)
+        plt.title(parameter)
+        plt.savefig(parameter+'.png')
+
+
+    def display_data(parameter):
+        rows = spark.sql('select '+parameter+' from pulse').collect()
+        dict = {}
+        for row in rows:
+            result = row.asDict()
+            array = result[parameter]
+            for item in array:
+                if item in dict.keys():
+                    dict[item] += 1
+                else:
+                    dict[item] = 1
+        print(dict)  
+        plot(dict, parameter)
+
+
+    def display_data_adversary():
+        rows = spark.sql("select adversary from pulse").collect()
+        dict = {}
+        for row in rows:
+            result = row.asDict()
+            print(result)
+            value = result["adversary"]
+            if value == "":
+                continue
+            if value in dict.keys():
+                dict[value] += 1
+            else:
+                dict[value] = 1
+        print(dict)
+        plot(dict, "adversary")
+
+
+    def display_data_tags():
+        rows = spark.sql("select tags from pulse").collect()
+        dict = {}
+        for row in rows:
+            result = row.asDict()
+            array = result["tags"]
+            for item in array:
+                if item in dict.keys():
+                    dict[item] += 1
+                else:
+                    dict[item] = 1
+        sorted_tuple_list = sorted(dict.items(), key=lambda x:x[1], reverse=True)    
+        sorted_tuple_list = sorted_tuple_list[:10]
+        print(sorted_tuple_list)
+
+        dict={}
+        for tuple in sorted_tuple_list:
+            dict[tuple[0]] = tuple[1]
+
+        print(dict)
+        plot(dict,"tags")
 
     
+    '''def malware_families_in_targeted_country():
+        ids = spark.sql("select id from pulse").collect()
+        dict = {}
+        dict_malwares_frequency = {}
+        for id in ids:
+            id_as_dict = id.asDict()
+            #print(id_as_dict)
+            value = id_as_dict.get('id')
+            #print(value)
+            targeted_countries = spark.sql('select targeted_countries from pulse WHERE id = "'+value+'"').collect()
+            malware_families = spark.sql('select malware_families from pulse WHERE id = "'+value+'"').collect()
+            for country in targeted_countries:
+                country = country.asDict()
+                if country in dict.keys():
+                    for malware in malware_families:
+                        malware = malware.asDict()
+                        if malware in dict_malwares_frequency.keys():
+                            dict_malwares_frequency[malware] += 1
+                            dict[country] = dict_malwares_frequency
+                        else:
+                            dict_malwares_frequency[malware] = 1
+                            dict[country] = dict_malwares_frequency
+                else:
+                    for malware in malware_families:
+                        if malware in dict_malwares_frequency.keys():
+                            dict_malwares_frequency[malware] += 1
+                            dict[country] = dict_malwares_frequency
+                        else:
+                            dict_malwares_frequency[malware] = 1
+                            dict[country] = dict_malwares_frequency
+        print(dict)
+                    
+    '''
+
+    #TARGETED COUNTRIES
+    display_data("targeted_countries")
     
+    #MALWARE FAMILIES
+    display_data("malware_families")
 
-   
-    for i in countries1:
-        for x in i:
-            for n in x:
-               if n in my_dictionary.keys():
-                   if my_dictionary[n] == None:
-                        my_dictionary[n] = 1
-                   else:
-                     my_dictionary[n] = my_dictionary[n] + 1
-                   
-    print(my_dictionary)           
+    #INDUSTRIES
+    display_data("industries")
+
+    #ADVERSARY
+    display_data_adversary()
+
+    #TAGS
+    display_data_tags()
     
-    group_data = list(my_dictionary.values())
-    group_names = list(my_dictionary.keys())
-    group_mean = np.mean(group_data) 
+    #NUMBER OF MALWARE FAMILIES IN TARGETED COUNTRIES
+    #malware_families_in_targeted_country()
 
-
-
-    fig, ax = plt.subplots()
-    ax.barh(group_names, group_data)
-    plt.show()
-    #print(numIndic)           #print(niz)
-    #.za svaki indicators ce uzeti njegov prvi indicator
-    #dataNEw = spark.sql("select indicators from puls").collect()
-   # print(dataNEw)
-    #spark.sql("select tlp from puls").show()
-    #spark.sql("select indicator from indicators from puls").show()
-    #spark.sql("select revision from puls").show
-#dataframe.printSchema()
-    # Print the DataFrame to the console
-   
 else:
     print("Error:", response.status_code)
 # Stop the SparkSession
